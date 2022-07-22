1639fcb683e8c7fb737213ea101cc886fa1ff0e3
Alfredo reza <alfredoreza@Alfredos-MacBook-Pro-2.local>
2022-07-22 12:52:47 -0500

    add working custom card component

--- src/x-853443-custom-card-component/index.js
+++ src/x-853443-custom-card-component/index.js
@@ -2,7 +2,7 @@ import {createCustomElement} from '@servicenow/ui-core';
 import snabbdom from '@servicenow/ui-renderer-snabbdom';
 import styles from './styles.scss';
 
-const view = ({properties}, dispatch) => {
+const view = ({properties}, {dispatch}) => {
 
 	const {
 		header1, 
@@ -14,22 +14,29 @@ const view = ({properties}, dispatch) => {
 		footer2,
 		footer3,
 		footer4,
-		sysId
+		logoImage,
+		sysId,
+		table
 	} = properties;
 
 	const handleClick = () => {
-		dispatch('CUSTOM_CARD_COMPONENT#CLICKED', {sysId: sysId})
+		dispatch('CUSTOM_CARD_COMPONENT#CLICKED', {sysId, table})
 	}
 
 	return (
-		<div className='cardContainer' on-click={handleClick}>
+		<div id='card-container' 
+			className='cardContainer' 
+			on-click={handleClick}
+			//  value={sysId}
+			>
 			<header className='headerContainer'>
-				<img src='https://images.squarespace-cdn.com/content/5aae96c455b02c9c8b54ad63/9fd5fd6e-3294-4468-af23-c49b241947ca/MicrosoftTeams-image+%283%29.png?format=1000w&content-type=image%2Fpng' height='50px' width='50px'/>
+				<img src={logoImage} height='50px' width='50px'/>
 				<div>
 					<div>{header1}</div>
 					<div>{header2}</div>
 				</div>
 			</header>
+			<div className='bottomHalf'>
 			<main className='mainContainer'>
 				<div className='mainTitle'>{mainTitle}</div>
 				<div>{mainSubheader1}</div>
@@ -41,6 +48,7 @@ const view = ({properties}, dispatch) => {
 				<div>{footer3}</div>
 				<div>{footer4}</div>
 			</footer>	
+			</div>
 		</div>
 	);
 };
@@ -55,16 +63,38 @@ createCustomElement('x-853443-custom-card-component', {
 		mainTitle: {default: 'Service Portal Feature Update'},
 		mainSubheader1: {default: ''},
 		mainSubheader2: {default: ''},
-		footer1: {default: ''},
+		footer1: {default: 'test'},
 		footer2: {default: ''},
 		footer3: {default: ''},
 		footer4: {default: ''},
-		logoImage: {defualt: ''},
+		logoImage: {default: ''},
 		sysId: {default: ''},
+		table: {default: ''},
 	},
 	actionHandlers:{
 		"CUSTOM_CARD_COMPONENT#CLICKED": ({action}) => {
 			console.log(action.payload);
 		}
-	}
+	},
+	// eventHandlers: [
+	// 	{
+	// 		events: ['click'],
+	// 		effect({dispatch, action: {payload: {event}}}) { 
+	// 			const {value} = event.path.find(el => el.id == 'card-container');
+	// 			console.log(value);
+	// 			console.log(event);
+	// 			dispatch("CUSTOM_CARD_COMPONENT#CLICKED", {sysId: value});
+	// 		}
+	// 	}
+		// {
+		// 	events: ['CUSTOM_CARD_COMPONENT#CLICKED'],
+		// 	// capture: true,
+		// 	// target: document.getElementById('card-container'),
+		// 	effect({dispatch, action: {payload: {event}}}) { 
+		// 		// const {value} = event;
+		// 		console.log(event);
+		// 		dispatch('CC_COLOR_PICKER#VALUE_SET', {value: 'test'});
+		// 	}
+		// }
+	// ]
 });
